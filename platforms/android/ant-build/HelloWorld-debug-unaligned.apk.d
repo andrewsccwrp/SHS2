4ad52c0316373fee828a0b3ff4d90edfa0ad6ae9
Paul Smith <pauls@sccwrp.org>
2014-07-09 14:48:42 -0700

    second commit

--- platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Library/WebServer/Documents/shs/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
-/Library/WebServer/Documents/shs/platforms/android/ant-build/HelloWorld.ap_ \
-/Library/WebServer/Documents/shs/platforms/android/ant-build/classes.dex \
-/Library/WebServer/Documents/shs/platforms/android/CordovaLib/ant-build/classes.jar \
